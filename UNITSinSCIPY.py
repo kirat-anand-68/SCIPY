dafc788eae8b540e7a295c5c5e891b4300942b06
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2024-12-13 00:28:46 +0530

    Create UNITSinSCIPY.py

---
+++ UNITSinSCIPY.py
@@ -0,0 +1,160 @@
+#Constants in SCIPY->it is more focused on scientific implementations it provide many built scientific constants
+#helpful in data science
+# printing the constant value of pi
+#It Focuses on the Scientific values
+# from scipy import constants
+# print(constants.pi)
+#All list of units under can be seen with the dir() function.
+#Unit Categories-> Matric,binary ,Mass,Angle,Length,Pressure,Volumes,Speed,temperature.energy,power,force
+#***************************Metric Unit->(SI)Prefix->It is used for the unit conversion************************
+from scipy import constants
+# print(constants.yotta)#1e+24
+# print(constants.zetta)#1e+21
+# print(constants.exa)#1e+18
+# print(constants.peta)#1000000000000000.0
+# print(constants.tera)#10000000000000.0
+# print(constants.giga)#1000000000.0
+# print(constants.mega)#1000000.0
+# print(constants.kilo)#1000.0
+# print(constants.hecto)#100.0
+# print(constants.deka)#10.0
+# print(constants.deci)#0.1
+# print(constants.centi)#0.01
+# print(constants.milli)#0.001
+# print(constants.micro)#1e-06
+# print(constants.nano)#1e-09
+# print(constants.pico)#1e--12
+# print(constants.femto)#1e-15
+# print(constants.atto)#1e-18
+# print(constants.zepto)#1e-21
+#**********print(constants.zetta)************************************BINARY PREFIXES*****************************************
+# print(constants.kibi)#1024
+# print(constants.tebi)#1099511627776
+# print(constants.mebi)#1048576
+# print(constants.pebi)#1125899906842624
+# print(constants.exbi)#1152921504606846976
+# print(constants.zebi)#1180591620717411303424
+# print(constants.yobi)#1208925819614629174706176
+#*************************************************MASS************************************************
+# print(constants.gram)
+# print(constants.metric_ton)
+# print(constants.grain)
+# print(constants.lb)
+# print(constants.pound)
+# print(constants.oz)
+# print(constants.ounce)
+# print(constants.stone)
+# print(constants.long_ton)
+# print(constants.short_ton)
+# print(constants.troy_ounce)
+# print(constants.troy_pound)
+# print(constants.carat)
+# print(constants.atomic_mass)
+# print(constants.m_u)
+# print(constants.u)
+#****************************************ANGLE****************************************************
+#ANGLE->
+# print(constants.degree)#0.0174
+# print(constants.arcminute)#0.0002
+# print(constants.arcmin)
+# print(constants.arcsec)
+# print(constants.arcsecond)
+#******************************************TIME*****************************************************
+#TIME->
+# print(constants.hour)
+# print(constants.minute)
+# print(constants.day)
+# print(constants.year)
+# print(constants.light_year)
+# print(constants.astronomical_unit)
+# print(constants.week)
+# print(constants.Julian_year)
+#*************************************************LENGTH************************************************
+#IT CONVERT ALL THINGS INTO THE METER......
+# print(constants.inch)
+# print(constants.foot)
+# print(constants.yard)
+# print(constants.mile)
+# print(constants.mil)
+# print(constants.pt)
+# print(constants.point)
+# print(constants.survey_foot)
+# print(constants.survey_mile)
+# print(constants.nautical_mile)
+# print(constants.fermi)
+# print(constants.angstrom)
+# print(constants.micron)
+# print(constants.au)
+# print(constants.light_year)
+# print(constants.parsec)
+#*********************************************PRESSURE******************************************8**
+#ALL THINGS CHANGED IN TO PASCAL
+# print(constants.atm)
+# print(constants.atmosphere)
+# print(constants.bar)
+# print(constants.torr)
+# print(constants.mmHg)
+# print(constants.psi)
+#*************************************************VOLUME******************************************
+#CONVERT ALL THE UNITS INTO THE CUBIC-METER->
+# print(constants.liter)#0.001
+# print(constants.litre)#0.001
+# print(constants.gallon)
+# print(constants.gallon_US)
+# print(constants.gallon_imp)
+# print(constants.fluid_ounce)
+# print(constants.fluid_ounce_US)
+# print(constants.fluid_ounce_imp)
+# print(constants.barrel)
+# print(constants.bbl)
+#**********************************************SPEED***********************************
+#IT IS IN METER/SEC
+# print(constants.kmh)
+# print(constants.mph)
+# print(constants.mach)
+# print(constants.speed_of_sound)
+# print(constants.knot)
+#******************************************TEMPERATURE***********************************
+# print(constants.zero_Celsius)
+# print(constants.degree_Fahrenheit)
+#*********************************************ENERGY**************************************************
+#MEASURES IN JOULES
+# print(constants.eV)
+# print(constants.electron_volt)
+# print(constants.calorie)
+# print(constants.erg)
+# print(constants.Btu)
+# print(constants.Btu_IT)
+# print(constants.Btu_th)
+# print(constants.ton_TNT)
+#************************************************POWER*******************************************
+#NEWTOn->
+print(constants.dyn)
+print(constants.dyne)
+print(constants.lbf)
+print(constants.pound_force)
+print(constants.kgf)
+print(constants.kilogram_force)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
